6fd82b886fee36746139008e74941b5f0bba7aa3
Unknown <dollapat.966@hotmail.com>
2019-04-30 08:50:25 +0700

    Update parser for generate 'else',' loop', 'end' ('if' incomplete)

--- cc_parser.py
+++ cc_parser.py
@@ -17,48 +17,69 @@ precedence = (
     ('left', 'L_PAREN', 'R_PAREN', 'L_ARRAY', 'R_ARRAY'),
 )
 
-# dictionary of variable names
-names = {}          # { 'n':3, 'ar':[1,2,3], ... }
-indexs = {}          
+# dictionary 
+names = {}      # variable names = { 'n':3, 'ar':[1,2,3], ... }
+indexs = {}     # loop indexs     
+strings = {}    # printed strings
+strlens = {}    # string lengths
+
+# temp before store to strings = {}
+strtemp = ''
 
 # counter and stack for asm label and loop index
+str_ct = 0
 lp_ct = 0
-idx_ct = 0
-end_ct = 0
+end_ct = {'if':0, 'lp':0}
 lp_stack = []
 end_stack = []
 
 # else checker
 else_checker = False 
 
-# if/else/loop match checker
-end_checker = []
-def mark_end(lineno):
-    end_checker.append(lineno)
+# # if/else/loop match checker
+# end_checker = []
+# def mark_end(lineno):
+#     end_checker.append(lineno)
 
-def unmark_end():
-    try:
-        end_checker.pop()
+# def unmark_end():
+#     try:
+#         end_checker.pop()
 
 # jump condition map sign (invert for short if)
 j_cond = {
-    '='  : 'JNE   ',
-    '!=' : 'JE    ',
-    '>'  : 'JLE   ',
-    '>=' : 'JL    ',
-    '<'  : 'JGE   ',
-    '<=' : 'JG    '
+    '='  : 'jne   ',
+    '!=' : 'je    ',
+    '>'  : 'jle   ',
+    '>=' : 'jl    ',
+    '<'  : 'jge   ',
+    '<=' : 'jg    ',
+
+    'inc': 'jg    ',
+    'dec': 'jl    ',
 }
 
-def jmp_end():
-    end_stack.append('end'+str(end_ct)+':')
-    end_ct += 1
+def jmp_end(type):
+    # type is if or lp(loop)
+    end_stack.append('end'+type+str(end_ct[type])+':')
+    end_ct[type] += 1
     return end_stack[-1][:-1]
 
-def label_end():
+def label_end(lineno):
     try:
         return end_stack.pop()
     except IndexError:
+        print("Line ({}) : Syntax error unexpected 'end'".format(lineno))
+
+def label_loop():
+    lp_stack.append('lp'+str(lp_ct))
+    lp_ct += 1
+    return lp_stack[-1]+':'
+
+def jmp_loop():
+    try:
+        return lp_stack.pop()
+    except IndexError:
+        pass    # no action, error same unexpected 'end'
 
 # for generate nasm code
 source_code = ''
@@ -116,31 +137,47 @@ def p_stm_assign_arr_index(t):
 def p_stm_if(t):
     '''stm : IF cond NEWLINE
            | ELSE IF cond NEWLINE'''
-    pass
+    # compare and jump are in cond
+    else_checker = True
 
 
 def p_stm_else(t):
     '''stm : ELSE NEWLINE'''
-    pass
-
+    if else_checker:
+        jmp_end('if')
+        else_checker = False
+    else:
+        print("Line ({}) : Syntax error found 'else' without 'if'".format(t.lineno))
 
 def p_stm_loop(t):
     '''stm : REPEAT expr TO expr INC expr NEWLINE
            | REPEAT expr TO expr DEC expr NEWLINE'''
-    if t[5] == 'inc':
-        pass
-    if t[5] == 'dec':
-        pass
+    indexs['idx'+str(lp_ct)] = t[2]
+    source_code +=  label_loop()+'\n'+\
+                    '   cmp     idx'+str(lp_ct-1)+', '+str(t[4])+'\n'+\
+                    '   '+j_cond[t[5]]+'      '+jmp_end('lp')+'\n'+\
+                    '   add     idx'+str(lp_ct-1)+', '+str(t[6])+'\n'
 
 
 def p_stm_end(t):
     '''stm : END NEWLINE'''
-    pass
+    end_label = label_end(t.lineno)
+    if 'lp' in end_label:
+        source_code +=  '   j       '+jmp_loop()+'\n'
+    source_code +=  '   '+end_label+'\n'
 
 
 def p_stm_print(t):
     '''stm : PRINT str NEWLINE'''
-    pass
+    strings['msg'+str(str_ct)] = strtemp
+    strlens['len'+str(str_ct)] = len(strtemp)
+    strtemp = ''
+    source_code +=  'mov    edx, len'+str(str_ct)+'\n'+\
+                    'mov    ecx, msg'+str(str_ct)+'\n'+\
+                    'mov    ebx, 1\n'+\
+                    'mov    eax, 4\n'+\
+                    'int    0x80\n'
+    str_ct += 1
 
 
 # expression
@@ -203,16 +240,15 @@ def p_cond_op(t):
             | expr DOWNWARD expr
             | expr DOWNWARD_EQUALS expr'''
     t[0] = (t[2], t[1], t[3])
-
-    if t[1][0] == 'INT':
-        t
-    elif t[1][0] == 'VAR':
-        pass
-    elif t[1][0] == 'ARR':
-        pass
-    source_code += 'CMP     ' #+ t[1] + 
-
-    source_code += j_cond[t[2]] + jmp_end() + '\n'
+    pass
+    # if t[1][0] == 'INT':
+    #     pass
+    # elif t[1][0] == 'VAR':
+    #     pass
+    # elif t[1][0] == 'ARR':
+    #     pass
+    # source_code += 'CMP     ' #+ t[1] + 
+    # source_code += j_cond[t[2]] + jmp_end() + '\n'
 
 
 def p_cond_expr(t):
@@ -241,13 +277,12 @@ def p_elem_many(t):
 def p_str(t):
     '''str : expr
            | STRING'''
-    pass
-    # print(t[1])
+    strtemp += str(t[1])
 
 
 def p_str_many(t):
     '''str : str SEPARATOR str'''
-    # no action
+    pass    # no action
 
 
 # error
